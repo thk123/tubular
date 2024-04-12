8133c964fdf8c9daf3bc92c2b2734e4178820740
Thomas <thekileyenator@gmail.com>
2024-04-12 21:54:09 +0100

    Reformat view file

--- src/view/chord_sequencer.rs
+++ src/view/chord_sequencer.rs
@@ -5,21 +5,18 @@ use crate::{data_types::tatum::Tatum, view_model::chord_sequencer_vm::ChordSeque
 pub(crate) fn update(vm: &mut ChordSequencerVm, ctx: &egui::Context, _frame: &mut eframe::Frame) {
     egui::CentralPanel::default().show(ctx, |ui| {
         ui.horizontal(|ui| {
-            for (index, chord) in vm
-                .chord_sequence()
-                .iter()
-                .enumerate()
-            {
+            for (index, chord) in vm.chord_sequence().iter().enumerate() {
                 let text = match chord {
                     Some(c) => c.to_string(),
                     None => ".".to_string(),
                 };
                 let centred_text = format!("{:^3}", text);
-                let (bg_colour, fg_colour) = if Tatum::try_from(index).unwrap() == vm.selected_chord() {
-                    (Color32::BLACK, Color32::WHITE)
-                } else {
-                    (Color32::WHITE, Color32::BLACK)
-                };
+                let (bg_colour, fg_colour) =
+                    if Tatum::try_from(index).unwrap() == vm.selected_chord() {
+                        (Color32::BLACK, Color32::WHITE)
+                    } else {
+                        (Color32::WHITE, Color32::BLACK)
+                    };
                 let rich_text = RichText::new(centred_text)
                     .background_color(bg_colour)
                     .color(fg_colour)
